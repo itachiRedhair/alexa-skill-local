867cfbdde14bc1944c2684170b15c257078ccff1
itachiRedhair <akshay.milmile@gmail.com>
2018-04-23 15:41:26 +0530

    tests for endpoint controller added

--- test/authServer.js
+++ test/authServer.js
@@ -9,10 +9,12 @@ const initAuthServer = require('./../authServer');
 const indexHtml = fs.readFileSync(path.resolve(__dirname, './../html/index.html'), 'utf8');
 const closeHtml = fs.readFileSync(path.resolve(__dirname, './../html/close.html'), 'utf8');
 
+
 describe("Testing Login With Amazon Page", function () {
     let response = null;
     let body = null;
     let httpServer = null;
+
     before(function (done) {
         httpServer = initAuthServer();
         request('http://localhost:3001', function (error, resp, respBody) {
@@ -22,6 +24,9 @@ describe("Testing Login With Amazon Page", function () {
         });
     });
 
+    after(function () {
+        httpServer.close();
+    });
 
     describe("Testing response statuscode and body", function () {
         it('should render the page with index.html', function () {
@@ -32,17 +37,13 @@ describe("Testing Login With Amazon Page", function () {
             expect(response.statusCode).to.equal(200);
         });
     })
-
-    after(function () {
-        httpServer.close();
-    });
-
 })
 
 describe("Testing 'Successful Authentication and now you can close page'", function () {
     let response = null;
     let body = null;
     let httpServer = null;
+
     before(function (done) {
         httpServer = initAuthServer();
         request('http://localhost:3001/oauth2/callback?access_token=your_login_with_amazon_access_token', function (error, resp, respBody) {
@@ -50,6 +51,10 @@ describe("Testing 'Successful Authentication and now you can close page'", funct
             body = respBody
             done();
         });
+    });
+
+    after(function () {
+        httpServer.close();
     });
 
     describe("Testing response statuscode and body", function () {
@@ -61,9 +66,4 @@ describe("Testing 'Successful Authentication and now you can close page'", funct
             expect(response.statusCode).to.equal(200);
         });
     })
-
-    after(function () {
-        httpServer.close();
-    });
-
 })
