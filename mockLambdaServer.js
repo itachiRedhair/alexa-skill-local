5ea24933ed3c0b263cf4371379e6009b304b5ee4
itachiRedhair <akshay.milmile@gmail.com>
2018-05-22 09:22:15 +0530

    fixed callback issues for ask sdk 2

--- mockLambdaServer.js
+++ mockLambdaServer.js
@@ -1,31 +1,40 @@
 #!/usr/bin / env node
 
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
-const colors = require('colors');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const colors = require("colors");
 
 const server = express();
 server.use(bodyParser.json());
-
 
 const filePath = process.argv[2];
 const port = process.argv[3];
 
 const handler = require(filePath).handler;
 
-server.post('/', (req, res) => {
-    // Create dummy lambda context with fail and succeed functions
-    const context = {
-        fail: () => res.sendStatus(500),
-        succeed: data => res.send(data),
-    };
+server.post("/", (req, res) => {
+  // Create dummy lambda context with fail and succeed functions
+  const context = {
+    fail: () => {
+      res.sendStatus(500);
+    },
+    succeed: data => {
+      res.send(data);
+    }
+  };
 
-    // Call handler function in lambda entry file
-    handler(req.body, context);
+  handler(req.body, context, (err, response) => {
+    if (err) {
+      console.log("data", err);
+      throw new Error(err);
+    }
+    if (response) {
+      res.send(response);
+    }
+  });
 });
 
-server.listen(port , function () {
-    console.log(colors.green('Mock Lambda Service is running on port ' + port))
+server.listen(port, function() {
+  console.log(colors.green("Mock Lambda Service is running on port " + port));
 });
